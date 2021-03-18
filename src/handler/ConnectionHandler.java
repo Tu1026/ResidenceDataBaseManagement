c647cedeb67c68479cf432dd144873f9ac1a7701
Jonathan Hirsch <jonat1996@gmail.com>
2021-03-17 20:41:14 -0700

    Added basic controller functions, passes connection to dataHandler if sucessfull

--- src/handler/ConnectionHandler.java
+++ src/handler/ConnectionHandler.java
@@ -1,11 +1,15 @@
 package handler;
 
+import model.ConnectionState;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionHandler {
     private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
-
+    private Connection connection;
+    private static boolean isLoggedIn = false;
 
 
     public ConnectionHandler() {
@@ -14,17 +18,48 @@ public class ConnectionHandler {
             // Note that the path could change for new drivers
             DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
         } catch (SQLException e) {
-            System.out.println("Error:" + " " + e.getMessage());
+            System.out.println("Error: initializing oracle library: " + e.getMessage());
         }
     }
 
 
-    public void login() {
+    /**
+     *
+     * @param username username string
+     * @param pwd password string
+     * @return ConnectionState with the connectionObject & the current state (logged in, Logged out)
+     */
+    public ConnectionState login(String username, String pwd) {
+        if (!isLoggedIn) {
+            try {
+                connection = DriverManager.getConnection(ORACLE_URL, username, pwd);
+                System.out.println(username + " logged in to oracle");
+                isLoggedIn = true;
+            } catch (SQLException e){
+                System.err.println("Error logging in: " + e.getMessage());
+            }
+        } else{
+            System.out.println("Error logging in: already logged in. \nClose the connection first and try again");
+        }
 
+        return new ConnectionState(connection, isLoggedIn);
     }
 
-    public void close() {
+    /**
+     * Closes connection if connection is not null
+     * User does not need to be logged in to call this
+     */
+    public ConnectionState close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            isLoggedIn = false;
+        } catch (SQLException e) {
+            System.out.println("Error closing connection: " + e.getMessage());
+        }
 
+        return new ConnectionState(connection, isLoggedIn);
     }
 
 
