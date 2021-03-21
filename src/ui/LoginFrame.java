3fec259b36de70b44fa22f8a5aab7cad509d5b85
Tu1026 <s31302@gmail.com>
2021-03-21 00:17:40 -0700

    each text feild is associated with an event now we just need to utilize the conneciton handler

--- src/ui/LoginFrame.java
+++ src/ui/LoginFrame.java
@@ -1,18 +1,31 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginFrame extends Application {
     Button button;
-    public static void main(String[] args) {
-        launch(args);
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField password;
+    @FXML
+    void logIntoDB(ActionEvent event) {
+        String name = userName.getText();
+        System.out.println("hello world");
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -22,5 +35,9 @@ public class LoginFrame extends Application {
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
