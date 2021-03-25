53aa3b21248266d51cf6a7b5139154e042957686
Tu1026 <s31302@gmail.com>
2021-03-24 22:23:21 -0700

    saving before merge

--- src/ui/MenuItems.java
+++ src/ui/MenuItems.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.tables.Campus;
 
 import java.io.IOException;
 
@@ -68,23 +69,23 @@ public class MenuItems extends Application {
 
         }
 
-//    @FXML
-//    private void handleButtonAction (ActionEvent event) throws Exception {
-//        Stage stage;
-//        Parent root;
-//
-//        if(event.getSource()==btn1){
-//            stage = (Stage) btn1.getScene().getWindow();
-//            root = FXMLLoader.load(getClass().getResource("FXML2.fxml"));
-//        }
-//        else{
-//            stage = (Stage) btn2.getScene().getWindow();
-//            root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-//        }
-//        Scene scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+        @FXML
+        private void goIntoTables (ActionEvent event) throws Exception {
+            Stage stage;
+            Parent root;
+
+            if(event.getSource()== CampusB){
+                stage = (Stage) CampusB.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("FXML2.fxml"));
+            }
+            else{
+                stage = (Stage) CampusB.getScene().getWindow();
+                root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+            }
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
 
     public static void main(String[] args) {
         launch(args);
