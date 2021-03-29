84cd1cdb28cbe8194ff00ee9d19fcf2c5a12db0e
Tu1026 <s31302@gmail.com>
2021-03-28 23:11:46 -0700

    testing the function of callback

--- src/ui/ResidentInsert.java
+++ src/ui/ResidentInsert.java
@@ -1,5 +1,6 @@
 package ui;
 
+import controller.Controller;
 import interfaces.ControllerDelegate;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -10,42 +11,61 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.table.Column;
+import model.table.Table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ResidentInsert extends Application {
 
     private ControllerDelegate controller;
     private Scene scene;
+    private List<String> Residence;
+    private String residenceName;
+    private String address;
+    private String zipcode;
 
-//
+
 //    public ResidentInsert(ControllerDelegate controller){
 //        this.controller = controller;
 //    }
 
+
     @Override
     public void start(Stage primaryStage) {
+        controller = new Controller();
+        controller.login("ora_linshuan","a41053539");
+        HashMap<String, String> testMap = new HashMap<>();
+        List<String> testList = new ArrayList<>();
+        testList.add("Residence Name");
+        testList.add("Address");
+        testList.add("Zipcode");
+        controller.getDataForStudentInsertion("RESIDENCE", testList, testMap, this::getResAddres);
         GridPane masterGridPane = new GridPane();
 
         Pane zeroPane = new Pane();
-        Label emailLabel = new Label();
+        Label emailLabel = new Label("E-mail");
         TextField emailText = new TextField();
+
         zeroPane.getChildren().addAll(emailText, emailLabel);
-        emailLabel
+        masterGridPane.add(zeroPane,0,0);
 
 
-
-        HBox emailBox = new HBox();
-//        TextField emailText =
-
-//        Scene newScene = new Scene(test, 1600,300);
-//        primaryStage.setScene(newScene);
-//        primaryStage.show();
+        Scene newScene = new Scene(masterGridPane, 1600,300);
+        primaryStage.setScene(newScene);
+        primaryStage.show();
     }
 
     public Scene getScene() {
         return scene;
-
-
         ///### Pass (HashMap(column:key, String, tableName)
     }
 
+    private void getResAddres(Table table){
+        for (Column column: table.getColumnsList()){
+            System.out.println(column.name);
+        }
+    }
 }
