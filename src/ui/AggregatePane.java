fc9ab3f10a3631ffa0e4084db6cbcf7aacb5ab5b
Tu1026 <s31302@gmail.com>
2021-03-31 00:40:13 -0700

    basic skeletion for combobox generaiton laid out

--- src/ui/AggregatePane.java
+++ src/ui/AggregatePane.java
@@ -9,7 +9,10 @@ import javafx.stage.Stage;
 import java.util.List;
 
 public class AggregatePane extends Application {
-    ComboBox<String> groupByBox = new ComboBox<>();
+    ComboBox<String> groupByCombo = new ComboBox<>();
+    ComboBox<String> aggregateFunctionCombo = new ComboBox<>();
+    ComboBox<String> columnsCanBePerformedCombo = new ComboBox<>();
+    ComboBox<String> columnsToBeDisplayedCombo = new ComboBox<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -24,10 +27,43 @@ public class AggregatePane extends Application {
     }
 
 
-    //Todo: Dynamically generate the combobox for group by
-    public ComboBox<String> groupByPossibilities(List<String> columns){
+    //Todo: Dynamically generate the combobox for groupBy
+    public void groupByCombo(List<String> columns){
+        groupByCombo.getItems().clear();
+        for(String column : columns){
+            groupByCombo.getItems().add(column);
+        }
+    }
 
-        return null;
+
+    /*
+     * User can choose from these aggregate functions can add more if needed
+     */
+    public void aggregateFunctionCombo(){
+        aggregateFunctionCombo.getItems().add("Sum");
+        aggregateFunctionCombo.getItems().add("Average");
+        aggregateFunctionCombo.getItems().add("Min");
+        aggregateFunctionCombo.getItems().add("Max");
+    }
+
+
+    //Todo: Given list of columns dynamically generate what columns these aggregate functions can perform on
+    //We can change it to ComboBoxItemWrap so we can select multiple columns?
+    public void columnsCanBePerformedCombo(List<String> columns) {
+        columnsCanBePerformedCombo.getItems().clear();
+        for (String column : columns) {
+            columnsCanBePerformedCombo.getItems().add(column);
+        }
+    }
+
+
+    //Todo: Given a list of columns that can be displayed when the selected aggrefateFunciton is used
+    //We could use ComboBoxItemWrap here but selecting multiple columns with different aggregate function is tricky?
+    public void columnsToBeDisplayedCombo(List<String> columns, String aggregateFunction) {
+        columnsToBeDisplayedCombo.getItems().clear();
+        for (String column : columns) {
+            columnsToBeDisplayedCombo.getItems().add(column);
+        }
     }
 
 
