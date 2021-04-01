e18cb0f8203acdbfaf4b2f6df7e73e255d19343c
Tu1026 <s31302@gmail.com>
2021-04-01 16:04:14 -0700

    halfway through adding labels

--- src/model/AdvanceQueries.java
+++ src/model/AdvanceQueries.java
@@ -2,10 +2,17 @@ package model;
 
 public enum AdvanceQueries {
     GROUPBY ("The average years of experience of RAs & SRAs per age group"),
-    NESTED ("The name of each house, # of students living alone, and years they have been living in res for all students who live alone and have been in that res at least as long as anyone else"),
+    NESTED ("For each house, the number of residents who live alone and have been in the residence the longest"),
     JOIN ("The # of student in each house who are older than <X>, their avg age, and the age of the oldest student"),
-    DIVISION("The house with all units that have capacity of 5"),
-    HAVING("The floors in houses that have more than <X> vacancies");
+    DIVISION("The house that has all the units with capacity of 5"),
+    HAVING("The floors in houses that have more than <X> vacancies"),
+    GROUPBYEXPLAIN("RAs, residence advisors, or SRA, senior residence advisors, are employees who work in the residence to support the residents. They could be long time employees who have " +
+            "worked for many years in the residence before but they could also be brand new employees that have never worked in residence before. " +
+            "Therefore, you can run this query to see for each of the age group what is the average years of experience among all the advisors"),
+    NESTEDXPLAIN(""),
+    JOINEXPLAIN("Students of different age are all able to live in residence. Using this query you can find of all the students older than <X> what is the average age and the oldest age in each house"),
+    DIVISIONEXPLAIN("Different units have different capacity. Using this query you can find the house that has 'all' of the units with capacity 5 and no other house has a unit with a capacity of 5"),
+    HAVINGEXPLAIN("A unit can have vacancies if it can still allow new residents to move in. This query will show you how all the floors that have more than <X> frequency in a house");
 
     private final String value;
 
--- src/ui/AdvanceSearchPane.java
+++ src/ui/AdvanceSearchPane.java
@@ -1,5 +1,6 @@
 package ui;
 
+import com.sun.rowset.internal.Row;
 import interfaces.ControllerDelegate;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -8,26 +9,44 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import model.AdvanceQueries;
 
-public class AdvanceSearchPane extends BorderPane {
+import javax.swing.*;
+
+public class AdvanceSearchPane extends GridPane {
     private final ComboBox<String> advanceCombo = new ComboBox<>();
     private final ControllerDelegate controller;
     private final TextField conditionField;
+    private TextArea queryExplaination;
 
     public AdvanceSearchPane(ControllerDelegate controller){
         this.controller = controller;
 
         Label lineLabel = new Label("Advanced Search");
+        Label queryLabel = new Label("  Detail explanation of this query:");
         setStyle("-fx-border-width: 1 0 0 0; -fx-border-color: #838181");
 
-        this.setTop(lineLabel);
+        //-------------------Set up constraints-----------------------//
+        RowConstraints topRow = new RowConstraints();
+        topRow.setPercentHeight(20);
+        RowConstraints midRow = new RowConstraints();
+        midRow.setPercentHeight(60);
+        ColumnConstraints firstColumn = new ColumnConstraints();
+        firstColumn.setPercentWidth(20);
+        ColumnConstraints secondColumn = new ColumnConstraints();
+        secondColumn.setPercentWidth(40);
+
+        this.getRowConstraints().addAll(topRow, midRow);
+        this.getColumnConstraints().addAll(firstColumn, secondColumn, secondColumn);
+
+
+        this.add(lineLabel,0,0);
         this.setPadding(new Insets(10,10,10,10));
 
         VBox column = new VBox(15);
 
-        HBox row1 = new HBox();
-        row1.getChildren().add(new Label("Find"));
+        VBox row1 = new VBox();
+        row1.getChildren().add(new Label("Find:"));
         row1.setSpacing(10);
-        row1.setFillHeight(true);
+//        row1.setFillHeight(true);
         row1.setAlignment(Pos.BASELINE_CENTER);
 
         VBox row2 = new VBox();
@@ -42,22 +61,43 @@ public class AdvanceSearchPane extends BorderPane {
 
 
         for(AdvanceQueries query: AdvanceQueries.values()){
-            advanceCombo.getItems().add(query.getText());
+            if(!query.toString().matches("\\D*[EXPLAIN]")){
+                advanceCombo.getItems().add(query.getText());
+                System.out.println(query.toString());
+            }
+               else
+                   System.out.println(query.toString());
         }
 
         advanceCombo.setMaxWidth(300);
         HBox.setHgrow(lineLabel, Priority.ALWAYS);
         row1.getChildren().add(advanceCombo);
+        conditionField.setMaxWidth(300);
 
         column.getChildren().addAll(row1,row2);
-        this.setCenter(column);
+        this.add(column,1,1);
 
+        VBox thirdColumnBox = new VBox();
+        queryExplaination = new TextArea("This area will give you a short description of what the advance query does");
+        queryExplaination.setEditable(false);
+        queryExplaination.setFocusTraversable(false);
+        queryExplaination.setWrapText(true);
+
+        thirdColumnBox.getChildren().addAll(new Label("What this query does exactly:"), queryExplaination);
+        queryExplaination.prefWidthProperty().bind(thirdColumnBox.widthProperty());
+        queryExplaination.prefHeightProperty().bind(thirdColumnBox.heightProperty());
+
+
+        this.add(queryExplaination,2,1);
+
+        this.setGridLinesVisible(true);
 
         //----------------Setting up action events-------------//
         advanceCombo.valueProperty().addListener((obs, oldItem, newItem) -> {
             if (oldItem == null || !oldItem.equals(newItem)) {
                 conditionField.setVisible(newItem.equals(AdvanceQueries.JOIN.getText()) ||
                         newItem.equals(AdvanceQueries.HAVING.getText()));
+                        updateQueryDescription(AdvanceQueries.getEnum(newItem));
             }
         });
 
@@ -67,6 +107,8 @@ public class AdvanceSearchPane extends BorderPane {
         runQuery.setOnAction(event -> {
             runIfSelected();
         });
+
+        this.add(queryLabel,2,0);
 
         BorderPane.setMargin(row1, new Insets(20,0,0,0));
 
@@ -105,4 +147,12 @@ public class AdvanceSearchPane extends BorderPane {
         }
         return true;
     }
+
+    private void updateQueryDescription(AdvanceQueries advance) {
+        String explaination = advance.toString() + "EXPLAIN";
+        queryExplaination.setText(AdvanceQueries.valueOf(explaination).getText());
+
+    }
+
+
 }
